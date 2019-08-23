211ecd6b92014cde3c7731277ec4a6f83a424e49
Mansour-Tumeh <mansou.tumeh@digitalcareerinstitute.org>
2019-08-23 21:19:34 +0200

    drag and drop 2

--- Javascript/26 DOM and BOM/Drag and Drop/example.js
+++ Javascript/26 DOM and BOM/Drag and Drop/example.js
@@ -7,15 +7,23 @@
 // }
 
 function start(e){
-    e.dataTransfer.setData("content",e.target.id);   
-
+    e.dataTransfer.setData("content",e.target.id);  
+    
 }
 function drop(e){
     e.preventDefault();
     let data = e.dataTransfer.getData("content");
     let img = document.getElementById(data);
     e.target.appendChild(img);
+    e.target.className="dragdrop";
 }
 function dragOver(e){
     e.preventDefault();
+}
+function dragEnter(e){
+    e.preventDefault();
+    e.target.className+=" hovering";
+}
+function dragLeave(e){
+    e.target.classList.remove("hovering");    
 }
