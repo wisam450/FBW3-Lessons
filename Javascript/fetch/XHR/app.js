4ff07dc5a9c4f3a1bc5dd48439336984f319c0fa
Mansour-Tumeh <mansou.tumeh@digitalcareerinstitute.org>
2019-09-24 12:18:02 +0200

    xml http request new changes

--- Javascript/fetch/XHR/app.js
+++ Javascript/fetch/XHR/app.js
@@ -9,51 +9,51 @@ function getJokes(e) {
     e.preventDefault();
 
     let number = document.querySelector("#number").value;
-    if(number ===''){
-        alert(" you should add a number");        
+    if (number === '') {
+        alert(" you should add a number");
     }
     else {
         console.log(number);
         let xhr = new XMLHttpRequest();
-        let url =  `http://api.icndb.com/jokes/random/${number}`
-        xhr.open('GET',url, true);
+        let url = `http://api.icndb.com/jokes/random/${number}`
+        xhr.open('GET', url, true);
         xhr.onload = function () {
             if (this.status === 200) {
                 //alert(this.status);
-                let response =JSON.parse(this.responseText) ;
+                let response = JSON.parse(this.responseText);
                 console.log(response);
                 let output = '';
-                if(response.type === "success"){
-                    response.value.forEach(function(item){
+                if (response.type === "success") {
+                    response.value.forEach(function (item) {
                         output += `<li>${item.joke}</li>`;
-    
+
                     })
-                    document.querySelector(".jokes").innerHTML= output;
-    
+                    document.querySelector(".jokes").innerHTML = output;
+
                 }
-    
+
             }
-    
+
         }
         xhr.send();
 
     }
-   
 
-} 
+
+}
 
 
 function jqueryJokes(e) {
 
     e.preventDefault();
-    let number = $("#number").val();   
+    let number = $("#number").val();
 
-    let url = `http://api.icndxftghdfb.com/jokes/random/${number}`;
+    let url = `http://api.icndb.com/jokes/random/${number}`;
 
-    $.get(url,function(data,status){
+    $.get(url, function (data, status) {
         let output = '';
-        if(status === "success"){
-            data.value.forEach(function(item){
+        if (status === "success") {
+            data.value.forEach(function (item) {
                 output += `<li>${item.joke}</li>`;
 
             })
@@ -62,9 +62,9 @@ function jqueryJokes(e) {
         }
 
     })
-    
+
 }
 
-function fetchJokes(){
+function fetchJokes() {
 
 }
