b6d89e48ef91cc146395fd0afd48d309354174f7
Mansour-Tumeh <mansou.tumeh@digitalcareerinstitute.org>
2019-09-24 12:10:42 +0200

    XMLHttpRequest

---
+++ Javascript/fetch/XHR/app.js
@@ -0,0 +1,70 @@
+document.querySelector(".get-jokes").addEventListener("click", getJokes);
+document.querySelector(".jquery-jokes").addEventListener("click", jqueryJokes);
+document.querySelector(".fetch-jokes").addEventListener("click", fetchJokes);
+
+
+
+function getJokes(e) {
+
+    e.preventDefault();
+
+    let number = document.querySelector("#number").value;
+    if(number ===''){
+        alert(" you should add a number");        
+    }
+    else {
+        console.log(number);
+        let xhr = new XMLHttpRequest();
+        let url =  `http://api.icndb.com/jokes/random/${number}`
+        xhr.open('GET',url, true);
+        xhr.onload = function () {
+            if (this.status === 200) {
+                //alert(this.status);
+                let response =JSON.parse(this.responseText) ;
+                console.log(response);
+                let output = '';
+                if(response.type === "success"){
+                    response.value.forEach(function(item){
+                        output += `<li>${item.joke}</li>`;
+    
+                    })
+                    document.querySelector(".jokes").innerHTML= output;
+    
+                }
+    
+            }
+    
+        }
+        xhr.send();
+
+    }
+   
+
+} 
+
+
+function jqueryJokes(e) {
+
+    e.preventDefault();
+    let number = $("#number").val();   
+
+    let url = `http://api.icndxftghdfb.com/jokes/random/${number}`;
+
+    $.get(url,function(data,status){
+        let output = '';
+        if(status === "success"){
+            data.value.forEach(function(item){
+                output += `<li>${item.joke}</li>`;
+
+            })
+            $(".jokes").html(output);
+
+        }
+
+    })
+    
+}
+
+function fetchJokes(){
+
+}
--- Javascript/fetch/fetch-example2/app.js
+++ Javascript/fetch/fetch-example2/app.js
@@ -4,95 +4,95 @@ let btn3 = document.querySelector("#btn3");
 let btn4 = document.querySelector("#btn4");
 let addPost = document.querySelector("#addPost");
 
-btn1.addEventListener("click",getText);
-btn2.addEventListener("click",getUsers);
-btn3.addEventListener("click",getPosts);
-btn4.addEventListener("click",allUsers);
-addPost.addEventListener("submit",createPost);
+btn1.addEventListener("click", getText);
+btn2.addEventListener("click", getUsers);
+btn3.addEventListener("click", getPosts);
+btn4.addEventListener("click", allUsers);
+addPost.addEventListener("submit", createPost);
 
 function getText() {
     fetch("test.txt")
-    .then(function (res) {
-        return res.text();
-      })
-    .then(function(data){
-        //console.log(data);
-        document.getElementById("output").innerText=data;
-
-    })
-    .catch(function (err) {
-        console.log(err);  
-    })
-  }
-  function getUsers() {
-      fetch("users.json")
-
-      .then(function (res) {
-          return res.json();
+        .then(function (res) {
+            return res.text();
         })
-      .then(function (data) {
-          console.log(data);
-          let output = "<h2 class='mb-4 text-danger'> Users </h2>";
-          data.forEach(function (user) {
-              output += `
+        .then(function (data) {
+            //console.log(data);
+            document.getElementById("output").innerText = data;
+
+        })
+        .catch(function (err) {
+            console.log(err);
+        })
+}
+function getUsers() {
+    fetch("users.json")
+
+        .then(function (res) {
+            return res.json();
+        })
+        .then(function (data) {
+            console.log(data);
+            let output = "<h2 class='mb-4 text-danger'> Users </h2>";
+            data.forEach(function (user) {
+                output += `
               <ul class="list-group mb-4"> 
                 <li class="list-group-item"> ${user.id}  </li>
                 <li class="list-group-item"> ${user.name}  </li>
                 <li class="list-group-item"> ${user.email}  </li>
               </ul>
-              `;              
-          });
-          document.getElementById("output").innerHTML = output; 
+              `;
+            });
+            document.getElementById("output").innerHTML = output;
 
-          
-      })
-      
-  }
 
-  function getPosts() {
+        })
+
+}
+
+function getPosts() {
     fetch("https://jsonplaceholder.typicode.com/posts")
 
-    .then(function (res) {
-        return res.json();
-      })
-    .then(function (data) {
-        console.log(data);
-        let output = "<h2 class='text-danger mb-4'> Posts </h2>";
-        data.forEach(function (post) {
-            output += `
+        .then(function (res) {
+            return res.json();
+        })
+        .then(function (data) {
+            console.log(data);
+            let output = "<h2 class='text-danger mb-4'> Posts </h2>";
+            data.forEach(function (post) {
+                output += `
             <div class="card card-body mb-4 " >
                <h3 class ="text-info"> ${post.title} </h3>
                <p> ${post.body} </p>
             </div>
-            `;              
-        });
-        document.getElementById("output").innerHTML = output; 
+            `;
+            });
+            document.getElementById("output").innerHTML = output;
 
-        
-    })
-    
+
+        })
+
 }
 // async await in js
 
 async function getAllUsers() {
-    
+
     //await response of fetch
     let response = await fetch("https://jsonplaceholder.typicode.com/users");
 
     //only proceed if the responce is resolved 
     let data = await response.json();
-   
+
     //only proceed if data is resolved 
-    return data;    
+    return data;
 }
 
-function allUsers(){
+function allUsers() {
 
-    getAllUsers().then(function(users) {
+    getAllUsers().then(function (users) {
 
         console.log(users);
         let output = "<h2 class='text-primary mt-4'> All users from API </h2>";
-        users.forEach(function(user){
+        users.forEach(function (user) {
             output += `
             <div class="card card-body mb-4 " >
                <h3 class=""> ${user.id} </h3>
@@ -101,33 +101,33 @@ function allUsers(){
                <p> street : ${user.address.street} </p>
                <p> city : ${user.address.city} </p>
             </div>
-            `; 
+            `;
         });
         document.getElementById("output").innerHTML = output;
-       
-       });
+
+    });
 }
 
 function createPost(e) {
     e.preventDefault();
     let title = document.querySelector("#title").value;
     let body = document.querySelector("#body").value;
-    fetch("https://jsonplaceholder.typicode.com/posts",{
-        method : 'POST',
-        headers : {
-            'Accept' : 'application/json , text/plain , */*',
-            'Content-type':'application/json'
+    fetch("https://jsonplaceholder.typicode.com/posts", {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json , text/plain , */*',
+            'Content-type': 'application/json'
         },
-        body:JSON.stringify( { title:title, body:body, userId:400} )
+        body: JSON.stringify({ title: title, body: body, userId: 400 })
 
     })
-    .then(function(res){
-        return res.json();           
-    })
-    .then(function (data) {
-        console.log(data);
-      })
+        .then(function (res) {
+            return res.json();
+        })
+        .then(function (data) {
+            console.log(data);
+        })
 
-    
 
-  }
+
+}
