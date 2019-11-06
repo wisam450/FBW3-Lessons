9a1b109226dab3d9d0bf0d98ee6d78a5e1e3c4bd
Sergey Epimakhov <sergey@dci>
2019-11-06 09:49:49 +0100

    basic redux

---
+++ React/Redux/react-redux-increment/increment_0/main.js
@@ -0,0 +1,29 @@
+// Redux 
+// - store
+// - reducer
+// - actions
+
+// reducer
+function counter (state = 0, action){
+    switch(action.type) {
+        case 'INCREMENT':
+            return state + 1;
+        case 'DECREMENT':
+            return state - 1;
+        default:
+            return state;
+    }
+}
+
+// store { subscribe, dispatch, getState }
+let store = Redux.createStore(counter); // expose store with registered counter
+
+// subscribe
+function printState() {
+    console.log(store.getState());
+}
+store.subscribe(printState);
+
+// actions
+store.dispatch({type: 'INCREMENT'})
+
