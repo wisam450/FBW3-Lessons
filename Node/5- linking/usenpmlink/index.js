fb22bb4419a3e13e6f440cdd0487c31b49c14af9
Mansour-Tumeh <mansou.tumeh@digitalcareerinstitute.org>
2019-11-28 08:28:40 +0100

    node js

---
+++ Node/5- linking/usenpmlink/index.js
@@ -0,0 +1,4 @@
+ const toupper = require('npmlink');
+
+ console.log(toupper('this text will be in upper case '));
+
--- Node/usenpmlink/index.js
+++
@@ -1,9 +0,0 @@
-// const toupper = require('npmlink');
-
-// console.log(toupper('this text will be in upper case '));
-console.log(process.argv);
-let stars = process.argv[2]
- 
-      process.stdout.write("*");
- 
- 
