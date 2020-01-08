192788cff0ac0be99ba0912fe760cf52d7222ccc
sergeyepimakhov <epimakhovsn@gmail.com>
2020-01-08 15:20:07 +0100

    put

--- Data Server/12-mongoose-intro/mongoose-demo/routes/products.js
+++ Data Server/12-mongoose-intro/mongoose-demo/routes/products.js
@@ -3,37 +3,48 @@ var router = express.Router();
 var Product = require('../models/Product');
 
 /* GET products listing. */
-router.get('/', async function(req, res, next) {
+router.get('/', async function (req, res, next) {
   // res.send('respond with a resource');
   try {
     const products = await Product.find();
-    res.status(200).send(products); 
+    res.status(200).send(products);
   } catch (e) {
     next(e);
   }
 });
 
 // GET http://localhost:3000/products/5e15bf6ce865442fc8748e2d
-router.get('/:productId', async function(req, res, next) {
+router.get('/:productId', async function (req, res, next) {
   // res.send(req.params.productId);
   let productId = req.params.productId;
   try {
     const products = await Product.findById(productId);
-    res.status(200).send(products); 
+    res.status(200).send(products);
   } catch (e) {
     next(e);
   }
 });
 
-// PUT http://localhost:3000/products/5e15bf6ce865442fc8748e2d?name=
-router.put('/:productId', async function(req, res, next) {
+// update info for a particular product
+// PUT http://localhost:3000/products/5e15bf6ce865442fc8748e2d?name='iPhone Y'&price=1000.0
+// curl -d "name='iPhone Y'&price=1000.0" -X PUT http://localhost:3000/products/5e15bf6ce865442fc8748e2d
+router.put('/:productId', async function (req, res, next) {
   // res.send(req.params.productId);
   let productId = req.params.productId;
+  let name = req.body.name;
+  let price = req.body.price;
+  console.log(name);
+  console.log(price);
   try {
-    const products = await Product.findByIdAndUpdate();
-    res.status(200).send(products); 
+    const products = await Product.findByIdAndUpdate(productId,
+      {
+        name: name,
+        price: price
+      });
+    res.status(200).send(products);
   } catch (e) {
     next(e);
   }
+});
 
-module.exports = router;
+  module.exports = router;
