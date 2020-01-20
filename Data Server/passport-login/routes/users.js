0b18752d71f7354e96d6d78c44b39bb11d1a1122
Mansour-Tumeh <mansou.tumeh@digitalcareerinstitute.org>
2020-01-20 15:11:26 +0100

    saveing the user to the database

--- Data Server/passport-login/routes/users.js
+++ Data Server/passport-login/routes/users.js
@@ -1,5 +1,10 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcryptjs');
+
+
+// User model 
+const User = require('../models/User');
 
 // Login -Page 
 router.get('/login', (req , res ) => {
@@ -39,7 +44,57 @@ router.post('/register' ,(req , res )=>{
        })
    }
    else {
-       res.send('ok');
+       // validation passed
+       User.findOne({email :email })
+       .then(data => {
+           if(data){ // we found the email in our database
+               errors.push({ msg : ' Email is already registered'});
+               res.render('register' ,{ 
+                   errors,
+                   name,
+                   email,
+                   password,
+                   password2
+               })               
+           }
+           else {
+               const newUser = new User({
+                   name,
+                   email,
+                   password
+               });
+
+               // Hash Password
+               bcrypt.genSalt(10 ,(err , salt) => {
+                   bcrypt.hash(newUser.password ,salt , (err , hash) => {
+                       if(err) throw err ;
+
+                       // set hashed password
+                       newUser.password = hash ;
+                       console.log('the hashed pass is : ' + newUser.password);
+                       // save the user data to our database
+                       newUser.save()
+                       .then(user => {
+                           res.redirect('/users/login');
+
+                       })
+                       .catch(err =>{ console.log(err);
+                       });
+
+
+
+                   } )
+
+
+               })
+
+
+
+
+           }
+
+       });
+
    }
 
 
