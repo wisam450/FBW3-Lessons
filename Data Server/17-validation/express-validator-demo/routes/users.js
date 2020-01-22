8ad86f399019fbbafaffbfd2390fdb6f82d2ee60
sergeyepimakhov <epimakhovsn@gmail.com>
2020-01-22 10:26:18 +0100

    validation next checks

--- Data Server/17-validation/express-validator-demo/routes/users.js
+++ Data Server/17-validation/express-validator-demo/routes/users.js
@@ -3,8 +3,14 @@ var router = express.Router();
 const { check, validationResult } = require('express-validator');
 
 /* GET users listing. */
-router.get('/', [
-  check('username').isEmail()
+router.post('/', [
+  check('username').isEmail(),
+  check('password').isLength({ min: 5}).isAlphanumeric(),
+  // check the date in format 'YYYY-MM-DD'
+  check('dateofbirth').isISO8601(),
+  check('age').isInt({min: 18, max: 120}),
+  check('mobile').isMobilePhone('de-DE'),
+  check('plz').isPostalCode('DE')
 ], function(req, res, next) {
   console.log(req.body);
   const errors = validationResult(req);
