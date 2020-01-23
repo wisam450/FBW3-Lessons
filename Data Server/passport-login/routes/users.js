589b131e4718f2e267de8a0f5417994fea56d793
Mansour-Tumeh <mansou.tumeh@digitalcareerinstitute.org>
2020-01-23 09:31:58 +0100

    update

--- Data Server/passport-login/routes/users.js
+++ Data Server/passport-login/routes/users.js
@@ -19,32 +19,6 @@ router.get('/register', (req , res ) => {
 });
 
 // Rigister Handle 
-<<<<<<< HEAD
-router.post('/register' ,(req , res )=>{
-   const { name, email, password, password2 }     = req.body;
-   let errors = [];
-   // checking the required fields
-   if(!name || !email || !password || !password2){
-       errors.push({ msg: 'please fill in all fields' });    
-
-   }
-   // checkking passwords match 
-   if(password !== password2){
-       errors.push({msg : 'Passwords do not match'});       
-   }
-   // checking pass length
-   if(password.length < 6 ){
-       errors.push({ msg  : "password should at least 6 characters"});
-   }
-   if(errors.length > 0){
-       res.render('register' , {
-           errors,
-           name,
-           email,
-           password,
-           password2
-       })
-   }
 
 const verifyPasswordsMatch = (req, res, next) => {
     const { password2 } = req.body;
