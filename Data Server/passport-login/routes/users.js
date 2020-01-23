ff5866fe8ee8c204c92fa0a437124e3cafb08314
Mansour-Tumeh <mansou.tumeh@digitalcareerinstitute.org>
2020-01-23 09:29:39 +0100

    update

--- Data Server/passport-login/routes/users.js
+++ Data Server/passport-login/routes/users.js
@@ -21,7 +21,6 @@ router.get('/register', (req , res ) => {
 router.post('/register' ,(req , res )=>{
    const { name, email, password, password2 }     = req.body;
    let errors = [];
-
    // checking the required fields
    if(!name || !email || !password || !password2){
        errors.push({ msg: 'please fill in all fields' });    
